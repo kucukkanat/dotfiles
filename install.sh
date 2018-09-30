bf6d8460baafa2ea0586541b1e498bb61c69374d
HILMI SAHIN <htolgasahin@gmail.com>
2018-09-30 21:03:58 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/kucukkanat/dotfiles ~/dotfiles
+cd -a ~/dotfiles/. ~/
+
+# Install vim plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
